473562bdc7f88d7de91ae645f274c10a316a7670
Ryo Koike <ryok022366@gmail.com>
2019-11-03 10:19:15 +0900

    foo

--- sim/preassemble.cc
+++ sim/preassemble.cc
@@ -76,7 +76,7 @@ void subst_labels(std::string infile, std::string outfile)
     if (!opcode.compare("jal")) ofs << opcode << " " << label_inst_list[v[1]] << "\n";
     else if (!opcode.compare("bc1f") || !opcode.compare("bc1t")) {
       //int n = label_inst_list[v[1]];
-      int n = label_inst_list[v[3]] - line_inst_list[l] - 1;
+      int n = label_inst_list[v[1]] - line_inst_list[l] - 1;
       ofs << opcode << " " << std::to_string(n) << "\n";
     }
     else if (!opcode.compare("j")) ofs << opcode << " " << label_inst_list[v[1]] << "\n";
