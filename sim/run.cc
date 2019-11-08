1910e36ea470dd34dda0def5fff9a06714d1659d
Ryo Koike <ryok022366@gmail.com>
2019-11-08 18:54:40 +0900

    foo

--- sim/run.cc
+++ sim/run.cc
@@ -121,9 +121,11 @@ uint32_t get_ra(uint32_t inst) {return (inst >> 16) & 0x1f;}
 uint32_t get_rb(uint32_t inst) {return (inst >> 11) & 0x1f;}
 uint32_t get_shift(uint32_t inst) {return (inst >> 6) & 0x1f;}
 int32_t get_shift_signed(uint32_t inst) {return (inst & 0x3ff) - (inst & 0x400);}
+//int32_t get_shift_signed(uint32_t inst) {return (int16_t)(inst & 0x3ff);}
 uint32_t get_func(uint32_t inst) {return (inst >> 0) & 0x3f;}
 uint16_t get_imm(uint32_t inst) {return (inst >> 0) & 0xffff;}
 int16_t get_imm_signed(uint32_t inst) {return (inst & 0x7fff) - (inst & 0x8000);}
+//int16_t get_imm_signed(uint32_t inst) {return (int16_t)(inst & 0x7fff);}
 uint32_t get_addr(uint32_t inst) {return (inst >> 0) & 0x3ffffff;}
 
 int main(int argc, char **argv)
